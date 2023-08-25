b643591474c72974c60425f14e8703a145f2a2f6
Akshay Patil <53465888+AkshayPatil4@users.noreply.github.com>
2023-08-26 00:31:45 +0200

    Adding deleteing and getiing data back from server

--- backend/app.js
+++ backend/app.js
@@ -1,58 +1,68 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose =require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const Post = require('./models/post');
+const Post = require("./models/post");
 const app = express();
 
-mongoose.connect("mongodb+srv://akshay:29kJCnZReMzmqWRr@cluster0.uybdzcj.mongodb.net/?retryWrites=true&w=majority").then(()=>{
-    console.log('Connected to Database');
-}).catch(()=>{
-    console.log('Connection failed');
-});
+mongoose
+  .connect(
+    "mongodb+srv://akshay:29kJCnZReMzmqWRr@cluster0.uybdzcj.mongodb.net/node-angular?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to Database");
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader("Access-Control-Allow-Methods","GET,POST,PATCH,DELETE,OPTIONS");
-    next();
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PATCH,DELETE,OPTIONS"
+  );
+  next();
 });
 
-app.post('/api/posts', (req,res,next)=>{
-    const post = new Post({
-        title: req.body.title,
-        content: req.body.content
-    });
-    console.log(post);
+app.post("/api/posts", (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save().then(createdPost=>{
     res.status(201).json({
-        message:'Post Added Successfully'
-    });
-
+        message: "Post Added Successfully",
+        postId: createdPost._id
+      });
+  });
+  
 });
 
 //29kJCnZReMzmqWRr
-app.get('/api/posts',(req , res, next)=>{
-    const posts=[
-        {
-            id: "sadafgafa",
-            title: "first server side post",
-            content: "this post is comming form server side"
-        },
-        {
-            id: "gsegsgsdg",
-            title: "Second server side post",
-            content: "this post is comming form server side"
-        },
-    ]
-
- res.status(200).json({
-        message: 'Posts fetched succefully',
-        posts: posts
+app.get("/api/posts", (req, res, next) => {
+  Post.find().then((documents) => {
+    console.log(documents);
+    res.status(200).json({
+      message: "Posts fetched succefully",
+      posts: documents,
     });
+  });
+});
+
+app.delete("/api/posts/:id", (req,res,next)=>{
+    Post.deleteOne({_id: req.params.id}).then(result =>{
+        console.log(result);
+        res.status(200).json({message:"Post deleted!"});
+    })
+    
 });
 
 module.exports = app;
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,32 +1,53 @@
-import { Subject } from "rxjs";
-import { Post } from "./post.model";
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { Subject, map } from 'rxjs';
+import { Post } from './post.model';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-@Injectable({providedIn: 'root'})
-export class PostService{
-   private posts: Post[] = [];
-   private postUpdated = new Subject<Post[]>();
-constructor(private Http: HttpClient){}
+@Injectable({ providedIn: 'root' })
+export class PostService {
+  private posts: Post[] = [];
+  private postUpdated = new Subject<Post[]>();
+  constructor(private Http: HttpClient) {}
 
-   getPosts(){
-    this.Http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts').subscribe((postData)=>{
-     this.posts= postData.posts;
-     this.postUpdated.next([...this.posts]);
+  getPosts() {
+    this.Http.get<{ message: string; posts:any }>(
+      'http://localhost:3000/api/posts'
+    ).pipe(map((postData)=>{
+      return postData.posts.map(post=>{
+         return{
+         title: post.title,
+         content: post.content,
+         id:post._id
+      };
+      });
+    })).subscribe(transformedposts => {
+      this.posts = transformedposts;
+      this.postUpdated.next([...this.posts]);
     });
-   }
+  }
 
-   getPostUpdateListner(){
+  getPostUpdateListner() {
     return this.postUpdated.asObservable();
-   }
+  }
 
-   addPost(title:string, content: string){
-        const post: Post = {id:null,title: title, content:content};
-        this.Http.post<{message:string}>('http://localhost:3000/api/posts', post).subscribe((responseData)=>{
-         console.log(responseData.message);
-         this.posts.push(post);
-         this.postUpdated.next([...this.posts]);
-        });
-       
-   }
+  addPost(title: string, content: string) {
+    const post: Post = { id: null, title: title, content: content };
+    this.Http.post<{ message: string , postId:string}>(
+      'http://localhost:3000/api/posts',
+      post
+    ).subscribe((responseData) => {
+      const id = responseData.postId;
+      post.id = id;
+      this.posts.push(post);
+      this.postUpdated.next([...this.posts]);
+    });
+  }
+
+  deletePost(postId:string){
+   this.Http.delete("http://localhost:3000/api/posts/" + postId).subscribe(()=>{
+      const updatedPosts = this.posts.filter(post => post.id !== postId);
+      this.posts=updatedPosts;
+      this.postUpdated.next([...this.posts]);
+   });
+  }
 }
