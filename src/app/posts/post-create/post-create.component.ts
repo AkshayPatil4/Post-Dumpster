31df380bbcf966b2b304fc09254add4c5da9e50a
Akshay Patil <53465888+AkshayPatil4@users.noreply.github.com>
2023-08-28 00:34:35 +0200

    added image and image preview but not to backend

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Output, OnInit } from '@angular/core';
-import { ActivatedRoute} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
-import { NgForm} from "@angular/forms"
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { PostService } from '../post.service';
 import { Post } from '../post.model';
 @Component({
@@ -14,42 +14,73 @@ export class PostCreateComponent implements OnInit {
   enteredTitle = '';
   enteredContent = '';
   isLoading = false;
+  form: FormGroup;
+  imagePreview:string;
   private mode = 'create';
-  private postId:string;
-   post :Post;
- 
-  constructor( public postService: PostService , public route: ActivatedRoute){}
+  private postId: string;
+  post: Post;
 
-ngOnInit(): void {
-  this.route.paramMap.subscribe((paramMap)=>{
-    if(paramMap.has('postId')){
-      this.mode= 'edit';
-      this.postId= paramMap.get('postId');
-      this.isLoading= true;
-      this.postService.getPost(this.postId).subscribe(postData =>{
-        this.isLoading=false;
-        this.post= {id: postData._id, title:postData.title, content: postData.content };
-      });
-    }
-    else{
-      this.mode='create';
-      this.postId= null;
-    }
-  });
-}
+  constructor(public postService: PostService, public route: ActivatedRoute) {}
 
-  onSavePost(form:NgForm) {
-    if (form.invalid){
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)],
+      }),
+      content: new FormControl(null, { validators: [Validators.required] }),
+      image: new FormControl(null, {validators:[Validators.required]})
+    });
+    this.route.paramMap.subscribe((paramMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.isLoading = true;
+        this.postService.getPost(this.postId).subscribe((postData) => {
+          this.isLoading = false;
+          this.post = {
+            id: postData._id,
+            title: postData.title,
+            content: postData.content,
+          };
+          this.form.setValue({
+            title: this.post.title,
+            content: this.post.content,
+          });
+        });
+      } else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
+  }
+
+  onImagePicked(event:Event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    console.log(file);
+    const reader  = new FileReader();
+    reader.onload = ()=>{
+      this.imagePreview =reader.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  onSavePost() {
+    if (this.form.invalid) {
       return;
     }
-    this.isLoading= true;
-    if (this.mode === 'create'){
-      this.postService.addPost(form.value.title, form.value.content);
+    this.isLoading = true;
+    if (this.mode === 'create') {
+      this.postService.addPost(this.form.value.title, this.form.value.content);
+    } else {
+      this.postService.updatePost(
+        this.postId,
+        this.form.value.title,
+        this.form.value.content
+      );
     }
-    else{
-      this.postService.updatePost(this.postId, form.value.title, form.value.content)
-    }
-    
-    form.resetForm();
+
+   this.form.reset();
   }
 }
